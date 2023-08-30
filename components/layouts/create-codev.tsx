bf4fbc03273c112bdc261b2fa7f57057eef0b981
Vaibhav Sisodiya <91018916+Vaib215@users.noreply.github.com>
2023-08-30 15:31:34 +0530

    Update create-codev.tsx

--- components/layouts/create-codev.tsx
+++ components/layouts/create-codev.tsx
@@ -60,8 +60,8 @@ export default function CreateCodevView({ languages }: Prop) {
             <SelectValue placeholder="Choose a Lang." />
           </SelectTrigger>
           <SelectContent className="h-72 overflow-y-auto">
-            {languages.map((item) => (
-              <SelectItem value={item.language}>
+            {languages.map((item, index) => (
+              <SelectItem key={index} value={item.language}>
                 {item.language.slice(0, 1).toLocaleUpperCase() +
                   item.language.slice(1)}
               </SelectItem>
